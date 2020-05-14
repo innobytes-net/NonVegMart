28605d36c5ae5a32c1cf9b2cb46dd0c0a510635a
Deven <devenbhalerao@gmail.com>
2020-05-14 18:38:19 +0530

    added User and User group database models

---
+++ backend/database/models/User_Schemas.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  _userGroupId: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  FirstName: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  LastName: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  Email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  Password: {
+    type: String,
+    required: true,
+  }
+});
+
+const user = mongoose.model('user', userSchema);
+module.exports = user;
