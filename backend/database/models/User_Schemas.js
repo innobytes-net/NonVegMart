7caddacb12352c4eb68089d59751e047f957f23d
Deven <devenbhalerao@gmail.com>
2020-05-15 18:40:37 +0530

    removed extra code

--- backend/database/models/User_Schemas.js
+++ backend/database/models/User_Schemas.js
@@ -30,32 +30,6 @@ const userSchema = new mongoose.Schema({
 
 const user = mongoose.model('user', userSchema);
 module.exports = user;
-=======
-  _userGroupId: {
-    type: mongoose.Types.ObjectId,
-    required: true,
-  },
-  FirstName: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  LastName: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  Email: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  Password: {
-    type: String,
-    required: true,
-  }
-});
 
 const user = mongoose.model('user', userSchema);
 module.exports = user;
->>>>>>> 28605d36c5ae5a32c1cf9b2cb46dd0c0a510635a
